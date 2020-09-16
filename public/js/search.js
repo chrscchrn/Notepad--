0ddd2282af75dadb241b7de75824364dca9b926d
chrscchrn <cochranschris@gmail.com>
2020-09-16 16:03:39 -0500

    adding html files for search, browse and notes

--- public/js/search.js
+++ public/js/search.js
@@ -1,7 +1,39 @@
 (() => {
     // const searchButton = $("");
+    // const keyword = $("");
     // const  = $("");
-    // const  = $("");
-    
+
+    const axios = require("axios");
+    let date = new Date();
+    // let date2 = new Date(date.setDate(date.getDate()));
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+    let fullDay = `${year}-${month}-${day}`;
+    console.log(fullDay);
+
+    try {
+        const results = axios.get(`http://newsapi.org/v2/everything?q=bitcoin&from=${fullDay}&sortBy=publishedAt&apiKey=${process.env.API_KEY}`)
+        console.log(results);
+        res.json({ data: results.data })
+    }
+    catch (data) { console.log(data) }
+
+
+    // const NewsAPI = require('newsapi');
+    // const newsapi = new NewsAPI('fd74db3423b5430780f46c67f89febb2');
+
+    // app.get("/api/search", async (req, res) => {
+    //     newsapi.v2.topHeadlines({
+    //         sources: 'bbc-news,the-verge',
+    //         q: 'business',
+    //         category: 'general',
+    //         language: 'en',
+    //         country: 'us'
+    //     }).then(results => {
+    //         console.log(results);
+    //         res.json({ data: results.data });
+    //     });
+    // });
     
 })()
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,25 +1,7 @@
 require("dotenv").config()
 const db = require("../models")
-// const axios = require("axios");
-let date = new Date()
-// let date2 = new Date(date.setDate(date.getDate()))
-let year = date.getFullYear()
-let month = date.getMonth()
-let day = date.getDate()
-let fullDay = `${year}-${month}-${day}`
-// const NewsAPI = require('newsapi');
-// const newsapi = new NewsAPI('fd74db3423b5430780f46c67f89febb2');
-console.log(fullDay)
 
 module.exports = (app) => {
-    // app.get("/api/browse", async (req, res) => {
-    //     try {
-    //         const results = await axios.get(`http://newsapi.org/v2/everything?q=bitcoin&from=${fullDay}&sortBy=publishedAt&apiKey=${process.env.API_KEY}`)
-    //         console.log(results);
-    //         res.json({ data: results.data })
-    //     }
-    //     catch (data) { console.log(data) }
-    // })
 
     app.post("/api/login", (req, res) => {
         // Sending back a password, even a hashed password, isn't a good idea
@@ -58,19 +40,6 @@ module.exports = (app) => {
         }
     });
     
-    // app.get("/api/search", async (req, res) => {
-    //     newsapi.v2.topHeadlines({
-    //         sources: 'bbc-news,the-verge',
-    //         q: 'business',
-    //         category: 'general',
-    //         language: 'en',
-    //         country: 'us'
-    //     }).then(results => {
-    //         console.log(results);
-    //         res.json({ data: results.data });
-    //     });
-    // });
-
     app.get("/api/notes", (req, res) => {
         //This will be a databse request to get all the difference notes and articles we saved. 
         res.json({ articles: ["articles go here"], notes: ["notes go here"] });
