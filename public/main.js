bbb6d8041863919a7c703acbe128ae7ce3644c39
mugen 430 <acampado.maverick@gmail.com>
2020-09-16 11:45:48 -0500

    added app.get for mainjs

--- public/main.js
+++ public/main.js
@@ -7,3 +7,28 @@ $(document).ready(() => {
         $("#api-response").text(JSON.stringify(res))
     })
 })
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../landing.html"));
+  });
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../login.html"));
+  });
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../main.html"));
+  });
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
+  });
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../main.html"));
+  });
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../gio/about.html"));
+  });
+
