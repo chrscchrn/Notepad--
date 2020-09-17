5d069e66d334efbb00bd1ea950980d28c5b8c54a
chlokins <cnpolansky@gmail.com>
2020-09-17 10:30:15 -0500

    adding seq., user.js

--- models/note.js
+++ models/note.js
@@ -1,17 +1,17 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Note = sequelize.define("Note", {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            len: [1]
         }
-      },
-      body: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-        len: [1]
-      }
     });
 
     // Note.associate = function(models) {
@@ -23,6 +23,6 @@ module.exports = function(sequelize, DataTypes) {
     //     }
     //   });
     // };
-  
+
     return Note;
 };
