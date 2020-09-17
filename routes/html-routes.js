4a47b7417d8534a2ebcd9e3f88a5f6b6b65eea4d
chlokins <cnpolansky@gmail.com>
2020-09-17 15:27:18 -0500

    saved-notes, html-routes, note.hand, notes are now being rendered onto the browser this is safe revert point

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,6 +13,7 @@ module.exports = (app) => {
 
     app.get("/notes", (req, res) => {
         db.Note.findAll({}).then(notes => {
+            console.log(notes)
             res.render("notes", {
                 data: notes
             })
