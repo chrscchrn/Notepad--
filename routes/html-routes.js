480d092e8591f902389eff0117655a38c751a04d
chrscchrn <cochranschris@gmail.com>
2020-09-17 19:35:07 -0500

    merging for unknown reasons

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,6 @@
 const path = require("path");
 const db = require("../models")
 
-
-
 module.exports = (app) => {
 
     app.get("/search", (req, res) => {
@@ -19,19 +17,23 @@ module.exports = (app) => {
             })
         })
     });
-
+    
     app.get("/work", (req, res) => {
         res.render("work", {
             data: "hello work"
         })
     });
 
-    // app.get("/login", function(req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/login.html"));
-    // });
+    app.get("/login", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/login.html"));
+    });
 
-    // app.get("/signup", function(req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/signup.html"));
-    // });
+    app.get("/signup", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+    });
+
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/login.html"));
+    });
 
 }
