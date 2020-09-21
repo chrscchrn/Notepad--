69c5beffafb119d45788da7307dbbac35c402ecf
Gio Talabong <gio.talabong@gmail.com>
2020-09-20 19:02:53 -0700

    responsive again

--- public/js/notes.js
+++ public/js/notes.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-const elements = Array.from(document.querySelectorAll('.fill'));
-
-function fill(item, index) {
-  
-  const bgColor = getComputedStyle(item).backgroundColor,
-  fillLayer = document.createElement('div');
-  fillLayer.classList.add('fill-layer');
-  item.style.backgroundColor = 'transparent';
-  item.style.position = 'relative';
-  item.style.overflow = 'hidden';
-  setTimeout(function() {
-    fillLayer.style.backgroundColor = bgColor;  
-    item.appendChild(fillLayer);
-  }, index * 1000);
-  
-}
-
-elements.forEach(fill);
-=======
 (() => {
     
     function saveLocal() {
@@ -36,4 +16,3 @@ elements.forEach(fill);
         saveLocal();
     });
 })()
->>>>>>> ef677b6f7da58428956abe330bc51691ea9bb91a
